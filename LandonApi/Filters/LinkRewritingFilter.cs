8499562ed6e5e9f61fea82489a82f2d648e2e49b
Jeffery Cosby <jcoz1968@hotmail.com>
2018-11-06 22:43:48 -0600

    LinkRewriter

---
+++ LandonApi/Filters/LinkRewritingFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonApi.Filters
+{
+    public class LinkRewritingFilter : IAsyncResultFilter
+    {
+        public Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
