6d2bc1f37b3a5794d7015d113b0efa11854e99b7
Jeffery Cosby <jcoz1968@hotmail.com>
2018-11-06 20:56:09 -0600

    dbcontext, room entity, model

---
+++ LandonApi/Data/HotelApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LandonApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonApi.Data
+{
+    public class HotelApiDbContext : DbContext
+    {
+        public DbSet<RoomEntity> Rooms { get; set; }
+
+        public HotelApiDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+    }
+}
--- LandonApi/Startup.cs
+++ LandonApi/Startup.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using LandonApi.Filters;
+﻿using LandonApi.Filters;
 using LandonApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using NSwag.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using LandonApi.Data;
 
 namespace LandonApi
 {
@@ -29,6 +24,11 @@ namespace LandonApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<HotelInfo>(Configuration.GetSection("Info"));
+
+            // use in-memory db for dev and testing
+            // TODO: swap for real in prod
+            services.AddDbContext<HotelApiDbContext>(options => options.UseInMemoryDatabase("landondb"));
+
             services.AddMvc(options => 
             {
                 options.Filters.Add<JsonExceptionFilter>();
