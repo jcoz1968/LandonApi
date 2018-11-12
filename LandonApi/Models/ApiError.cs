bb3527ec294099172477960077d4de70d33315ee
Jeffery Cosby <jcoz1968@hotmail.com>
2018-11-12 15:42:30 -0600

    paging links

--- LandonApi/Models/ApiError.cs
+++ LandonApi/Models/ApiError.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace LandonApi.Models
 {
     public class ApiError
     {
+        private ModelStateDictionary modelState;
+
+        public ApiError()
+        {
+
+        }
+
+        public ApiError(ModelStateDictionary modelState)
+        {
+            Message = "Invalid parameters.";
+            Detail = modelState
+                .FirstOrDefault(x => x.Value.Errors.Any()).Value.Errors
+                .FirstOrDefault().ErrorMessage;
+        }
         public string Message { get; set; }
 
         public string Detail { get; set; }
--- LandonApi/Startup.cs
+++ LandonApi/Startup.cs
@@ -37,6 +37,8 @@ namespace LandonApi
         {
             services.Configure<HotelInfo>(Configuration.GetSection("Info"));
             services.Configure<HotelOptions>(Configuration);
+            services.Configure<PagingOptions>(Configuration.GetSection("DefaultPagingOptions"));
+
 
             services.AddScoped<IRoomService, DefaultRoomService>();
             services.AddScoped<IOpeningService, DefaultOpeningService>();
@@ -82,6 +84,15 @@ namespace LandonApi
 
             services.AddAutoMapper(
                 options => options.AddProfile<MappingProfile>());
+
+            services.Configure<ApiBehaviorOptions>(options => 
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errorResponse = new ApiError(context.ModelState);
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
